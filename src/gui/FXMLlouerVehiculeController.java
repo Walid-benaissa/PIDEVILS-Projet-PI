4c090357fbb4306b736f1bbeb3ba0e857ba18120
Azizi Khaled <azizi.khaled@esprit.tn>
2023-02-25 22:43:06 +0100

    mise a jour

--- src/gui/FXMLlouerVehiculeController.java
+++ src/gui/FXMLlouerVehiculeController.java
@@ -6,17 +6,23 @@
 package gui;
 
 import entities.Vehicule;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -56,6 +62,16 @@ public class FXMLlouerVehiculeController implements Initializable {
     private void rechercher(ActionEvent event) {
       
     
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/gui/FXMLVehicule.fxml"));
+            
+            Scene sc = new Scene(root);
+            Stage stage =(Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.setScene(sc);
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLlouerVehiculeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     
 }
 }
