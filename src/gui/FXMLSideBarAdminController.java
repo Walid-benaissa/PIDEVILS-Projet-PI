fa07a66f52de3649c7d1dc06968d79e48ab5bc84
Abir-Kharmachi <abir.kharmachi@esprit.tn>
2023-02-27 21:57:25 +0100

    sidebar

---
+++ src/gui/FXMLSideBarAdminController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author USER
+ */
+public class FXMLSideBarAdminController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void routeGererUser(ActionEvent event) {
+    }
+
+    @FXML
+    private void routeGererReclamation(ActionEvent event) {
+    }
+
+    @FXML
+    private void routeGererLivraisions(ActionEvent event) {
+    }
+
+    @FXML
+    private void routeGererVoitures(ActionEvent event) {
+    }
+
+    @FXML
+    private void routeConsulterLocation(ActionEvent event) {
+    }
+    
+}
