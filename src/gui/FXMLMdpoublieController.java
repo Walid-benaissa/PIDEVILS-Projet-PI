1a7038f34b5ed1d584896575a41a5b3693b69ef0
Abir-Kharmachi <abir.kharmachi@esprit.tn>
2023-03-02 16:27:07 +0100

    Bouton retour

--- src/gui/FXMLMdpoublieController.java
+++ src/gui/FXMLMdpoublieController.java
@@ -6,21 +6,26 @@
 package gui;
 
 import entities.Utilisateur;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import service.UtilisateurService;
+import utils.CommonController;
+import static utils.CommonController.setSceneContent;
 
 /**
  * FXML Controller class
  *
  * @author USER
  */
-public class FXMLMdpoublieController implements Initializable {
+public class FXMLMdpoublieController extends CommonController implements Initializable {
 
     @FXML
     private TextField fxLog;
@@ -43,6 +48,15 @@ public class FXMLMdpoublieController implements Initializable {
     private void EnvoyerBTN(ActionEvent event) {
         Utilisateur u = uc.rechUtilisateurByMail(fxLog.getText());
         String code = uc.sendMail(u.getMail());
+    }
+
+    @FXML
+    private void retour(ActionEvent event) {
+         try {
+            setSceneContent("FXMLAuthentification");
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLAuthentificationController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
